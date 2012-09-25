2ec3b2956cf22541cee4e478bd643e123f9891eb
Qiangning Hong <hongqn@douban.com>
2012-09-26 00:16:46 +0800

    finish

--- notes2mbox.py
+++ notes2mbox.py
@@ -5,6 +5,7 @@ import argparse
 import mailbox
 import email.message
 import email.header
+from email.mime.text import MIMEText
 from datetime import datetime, timedelta
 
 def convert(db_path, mbox_path, device_name):
@@ -15,15 +16,16 @@ def convert(db_path, mbox_path, device_name):
     curs = conn.cursor()
     curs.execute("SELECT * FROM ZNOTE")
     for row in curs.fetchall():
-        msg = email.message.Message()
-        dt = datetime.fromtimestamp(row['ZMODIFICATIONDATE'])
-        modtime = datetime(year=dt.year+31, month=dt.month, day=dt.day,
-                           hour=dt.hour, minute=dt.minute, second=dt.second)
-        modtime += timedelta(days=1)
-        msg.set_unixfrom("From %s %s" % (device_name, modtime.ctime()))
+        curs.execute("SELECT * FROM ZNOTEBODY WHERE Z_PK=?", (row['Z_PK'],))
+        content = curs.fetchone()['ZCONTENT'].encode('utf8')
+        msg = MIMEText(content, 'html', 'utf8')
+
+        msg.set_unixfrom("From %s %s" % (device_name,
+                                         shift_date(row['ZMODIFICATIONDATE']).ctime()))
         msg['From'] = device_name
         subject = row['ZTITLE']
         print subject
+        print row['ZSUMMARY']
         try:
             subject.encode('ascii')
         except UnicodeEncodeError:
@@ -32,8 +34,24 @@ def convert(db_path, mbox_path, device_name):
             except UnicodeEncodeError:
                 subject = email.header.Header(subject.encode('utf8'), 'UTF-8')
         msg['Subject'] = subject
-        msg.set_charset('base64')
+        msg['X-Universally-Unique-Identifier'] = row['ZGUID']
+        msg.set_charset('utf-8')
+        msg['X-Uniform-Type-Identifier'] = 'com.apple.mail-note'
+        msg['MIME-Version'] = "1.0 (Apple Message framework v1244.3)"
+        msg['X-Mail-Created-Date'] = rfc_2822(shift_date(row['ZCREATIONDATE']))
+        msg['Date'] = rfc_2822(shift_date(row['ZMODIFICATIONDATE']))
+        msg['X-Mail-Generated-Subject'] = 'YES'
+
         mbox.add(msg)
+
+def shift_date(timestamp):
+    dt = datetime.fromtimestamp(timestamp)
+    dt = datetime(year=dt.year+31, month=dt.month, day=dt.day, hour=dt.hour,
+                  minute=dt.minute, second=dt.second)
+    return dt + timedelta(days=1)
+
+def rfc_2822(dt):
+    return dt.strftime("%a, %d %b %Y %H:%M:%S +0800")
 
 def main():
     parser = argparse.ArgumentParser()
